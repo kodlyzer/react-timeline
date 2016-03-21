2c22f8f46523bb97d30aed16eb70fcf797999679
Karthik Rana <karthikr@qburst.com>
2016-03-21 20:51:33 +0530

    adds scss support

--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,14 @@ module.exports = {
                 plugins: ['transform-runtime'],
                 presets: ['es2015', 'react', 'stage-0'],
             }
+        }, {
+            loader: 'style!css!sass',
+            include: path.join(__dirname, 'src'),
+            test: /\.scss$/
         }]
     },
 
-    plugins : [],
+    plugins: [],
     output: {
         path: path.join(__dirname, 'build/js'),
         publicPath: '/build/',
